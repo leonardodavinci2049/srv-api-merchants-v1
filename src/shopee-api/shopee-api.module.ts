86a980bbd8f9144898c811683814d633a20c5246
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-07-12 19:31:21 -0300

    refactor: remove unused controllers from db-operation and shopee-api modules

--- src/shopee-api/shopee-api.module.ts
+++ src/shopee-api/shopee-api.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ShopeeApiController } from './shopee-api.controller';
 import { ShopeeApiService } from './shopee-api.service';
 
 @Module({
-  controllers: [ShopeeApiController],
   providers: [ShopeeApiService],
   exports: [ShopeeApiService],
 })
